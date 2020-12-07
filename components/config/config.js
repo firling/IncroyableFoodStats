3738def3845eb537d2d79840afe08842ac073131
Julien Anquetil <anquetil.jul@gmail.com>
2020-12-07 11:52:11 +0100

    build apk

--- components/config/config.js
+++ components/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    //url_serv: "http://localhost:3001",
-    url_serv: "http://192.168.1.43:3001",
+    url_serv: "http://51.68.122.81:3100",
+    //url_serv: "http://192.168.1.43:3001",
     STORAGE_KEY: "uwuAZEOIJAZLKJN"
 }
